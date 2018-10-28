Havoc-OS/android_packages_apps_Settings

Ambient Play: Allow to configure the scan interval [2/2]

src/com/android/settings/AmbientPlaySettings.java
@@ -22,6 +22,7 @@ import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.havoc.AmbientPlayPreferenceController;
 import com.android.settings.havoc.AmbientPlayKeyguardPreferenceController;
+import com.android.settings.havoc.AmbientPlayIntervalPreferenceController;
 
 public class AmbientPlaySettings extends DashboardFragment {
     private static final String TAG = "AmbientPlaySettings";
@@ -31,6 +32,7 @@ public class AmbientPlaySettings extends DashboardFragment {
         super.onAttach(context);
         addPreferenceController(new AmbientPlayPreferenceController(context));
         addPreferenceController(new AmbientPlayKeyguardPreferenceController(context));
+        addPreferenceController(new AmbientPlayIntervalPreferenceController(context));
     }
 
     @Override

Saksham033/packages_apps_Settings

Change-Id: I9b402aee2dbdb1a1faa1584dfc8ff616ace1aa4b

src/com/android/settings/AmbientPlaySettings.java
@@ -20,8 +20,9 @@ import android.content.Context;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.dashboard.DashboardFragment;
-import com.android.settings.havoc.AmbientPlayPreferenceController;
-import com.android.settings.havoc.AmbientPlayKeyguardPreferenceController;
+import com.android.settings.display.AmbientPlayPreferenceController;
+import com.android.settings.display.AmbientPlayKeyguardPreferenceController;
+import com.android.settings.display.AmbientPlayIntervalPreferenceController;
 
 public class AmbientPlaySettings extends DashboardFragment {
     private static final String TAG = "AmbientPlaySettings";
@@ -31,11 +32,12 @@ public class AmbientPlaySettings extends DashboardFragment {
         super.onAttach(context);
         addPreferenceController(new AmbientPlayPreferenceController(context));
         addPreferenceController(new AmbientPlayKeyguardPreferenceController(context));
+        addPreferenceController(new AmbientPlayIntervalPreferenceController(context));
     }
 
     @Override
     public int getMetricsCategory() {
-        return MetricsEvent.HAVOC_SETTINGS;
+        return MetricsEvent.DISPLAY_SETTINGS;
     }
 
     @Override